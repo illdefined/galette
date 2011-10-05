8e8a1f4bb10e23b282c9fa9703eb0d2a13274107
Mikael Voss <mikael@illdefined.org>
2011-10-05 10:04:59 +0000

    Enable additional hardening flags

--- galette.sh
+++ galette.sh
@@ -12,14 +12,48 @@ else
 	bin="${self#galette-}"
 fi
 
+# Enable flags
+fortify=1
+ssp=1
+pic=1
+pie=1
+combreloc=1
+relro=1
+now=1
+
+# Process command-line arguments
 for arg in "$@"
 do
 	case "$arg" in
-	(-fPIC|-fpic|-fPIE|-fpie|-fno-PIC|-fno-pic|-fno-PIE|-fno-pie|-static|-shared|-i|-r|-pie|-nopie)
-		exec "$bin" "$@";;
+	(-fstack-protector|-fstack-protector-all|-fno-stack-protector|-fno-stack-protector-all)
+		unset ssp;;
+	(-D_FORTIFY_SOURCE|-D_FORTIFY_SOURCE=*|-U_FORTIFY_SOURCE)
+		unset fortify;;
+	(-fPIC|-fpic|-fPIE|-fpie|-fno-PIC|-fno-pic|-static|-i|-r|-pie)
+		unset pic pie;;
+	(-fno-PIE|-fno-pie|-shared|-nopie)
+		unset pie;;
+	(-Wl,-z,combreloc|-Wl,-z,nocombreloc)
+		unset comreloc;;
+	(-Wl,-z,relro|-Wl,-z,norelro)
+		unset relro;;
+	(-Wl,-z,lazy|-Wl,-z,now)
+		unset now;;
 	(--)
 		break;;
 	esac
 done
 
-exec "$bin" -fPIE -pie "$@"
+# No PIC if PIE
+[ $pie ] && unset pic
+
+# Launch the compiler binary
+exec "$bin" \
+	${ssp:+-fstack-protector-all} \
+	${fortify:+-D_FORTIFY_SOURCE=2 -O} \
+	${pic:+-fPIC} \
+	${pie:+-fPIE -pie} \
+	${combreloc:+-Wl,-z,combreloc} \
+	${relro:+-Wl,-z,relro} \
+	${now:+-Wl,-z,now} \
+	"$@"
