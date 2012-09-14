4b463a4c1192d8849ddda1e39f4effdcf9a87d95
Mikael Voss <mvs@illdefined.org>
2012-09-14 00:05:08 +0000

    Pass -Wl,-pie when linking with clang

--- galette.sh
+++ galette.sh
@@ -12,7 +12,15 @@ else
 	bin="${self#galette-}"
 fi
 
+if [ "${bin%++}" == "clang" ]
+then
+	clang=1
+else
+	gcc=1
+fi
+
 # Enable flags
+link=1
 fortify=1
 ssp=1
 pic=1
@@ -26,12 +34,12 @@ for arg in "$@"
 do
 	case "$arg" in
 	(-c)
-		unset combreloc relro now;;
+		unset link;;
 	(-fstack-protector|-fstack-protector-all|-fno-stack-protector|-fno-stack-protector-all)
 		unset ssp;;
 	(-D_FORTIFY_SOURCE|-D_FORTIFY_SOURCE=*|-U_FORTIFY_SOURCE)
 		unset fortify;;
-	(-fPIC|-fpic|-fPIE|-fpie|-fno-PIC|-fno-pic|-static|-i|-r|-pie)
+	(-fPIC|-fpic|-fPIE|-fpie|-fno-PIC|-fno-pic|-static|-i|-r|-Wl,-pie|-pie)
 		unset pic pie;;
 	(-fno-PIE|-fno-pie|-shared|-nopie)
 		unset pie;;
@@ -54,8 +62,8 @@ exec "$bin" \
 	${ssp:+-fstack-protector-all} \
 	${fortify:+-D_FORTIFY_SOURCE=2 -O} \
 	${pic:+-fPIC} \
-	${pie:+-fPIE -pie} \
-	${combreloc:+-Wl,-z,combreloc} \
-	${relro:+-Wl,-z,relro} \
-	${now:+-Wl,-z,now} \
+	${pie:+-fPIE${clang:+${link:+ -Wl,-pie}}${gcc:+ -pie}} \
+	${link:+${combreloc:+-Wl,-z,combreloc}} \
+	${link:+${relro:+-Wl,-z,relro}} \
+	${link:+${now:+-Wl,-z,now}} \
 	"$@"
